d57893a9d6008fe256f96fc1ae1777fa8332c305
Erin <erindale.cpp@gmail.com>
2026-04-19 00:58:15 +0100

    Hidden camera position property

--- RenderDuckEngine/RenderDuckEngine/CameraSettings.h
+++ RenderDuckEngine/RenderDuckEngine/CameraSettings.h
@@ -3,6 +3,6 @@
 #include "Settings.h"
 
 PROPERTY_CONFIG_BEGIN(CameraSettings)
-	PROPERTY(float3, CameraPosition, float3(0, 0, -5))
+	PROPERTY_HIDDEN(float3, CameraPosition, float3(0, 0, -5))
 	PROPERTY(float, CameraSpeed, 7.0f)
 PROPERTY_CONFIG_END
